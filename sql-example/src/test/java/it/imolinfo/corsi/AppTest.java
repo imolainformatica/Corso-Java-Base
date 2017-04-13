854d8ec220539a9354b116e14c5ad62c08817f92
gmorlini <gmorlini@imolinfo.it>
2017-04-13 12:57:28 +0200

    esempio connesione jdbc

---
+++ sql-example/src/test/java/it/imolinfo/corsi/AppTest.java
@@ -0,0 +1,51 @@
+package it.imolinfo.corsi;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.sql.SQLException;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        App app = new App();
+        System.out.println( "Hello Db!" );
+        try {
+            app.connectionToDerby();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            app.normalDbUsage();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
